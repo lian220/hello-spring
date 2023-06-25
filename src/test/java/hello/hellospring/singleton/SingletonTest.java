d334d338200a99d85ea555c7d5d4747b5a0092c0
lian <lian.dy220@gmail.com>
2023-06-25 19:51:10 +0900

    test 싱글톤

--- src/test/java/hello/hellospring/singleton/SingletonTest.java
+++ src/test/java/hello/hellospring/singleton/SingletonTest.java
@@ -1,4 +1,4 @@
-package hello.hellospring;
+package hello.hellospring.singleton;
 
 import hello.hellospring.core.AppConfig;
 import hello.hellospring.core.singleton.SingletonService;
@@ -6,6 +6,8 @@ import hello.hellospring.service.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,4 +38,17 @@ public class SingletonTest {
         assertThat(singletonService1).isSameAs(singletonService2);
     }
 
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
---
+++ src/test/java/hello/hellospring/singleton/StatefulServiceTest.java
@@ -0,0 +1,39 @@
+package hello.hellospring.singleton;
+
+import hello.hellospring.core.AppConfig;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingeton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+//        threadA
+        int userAprice = statefulService1.order("userA", 10000);
+//        thireadB
+        int userBprice = statefulService1.order("userB", 20000);
+
+
+//        int price = statefulService1.getPrice();
+        System.out.println("price = " + userAprice);
+        assertThat(userAprice).isNotEqualTo(userBprice);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+
+}
